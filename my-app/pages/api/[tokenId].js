fc3285a104815a3a333e3ed85a7e190bd17c7893
deprismyproff <kookiedd@mail.ru>
2022-09-28 16:43:31 +0600

    huh

--- my-app/pages/api/[tokenId].js
+++ my-app/pages/api/[tokenId].js
@@ -3,13 +3,13 @@ export default function handler(req, res) {
     const tokenId = req.query.tokenId;
     // As all the images are uploaded on github, we can extract the images from github directly.
     const image_url =
-      "C:/Users/Пользователь/Desktop/NFT-Collection/my-app/public/our-collection/";
+      "https://github.com/deprismyproff/NFT-Collection/tree/main/my-app/public/our-collection/";
     // The api is sending back metadata for a Crypto Dev
     // To make our collection compatible with Opensea, we need to follow some Metadata standards
     // when sending back the response from the api
     // More info can be found here: https://docs.opensea.io/docs/metadata-standards
     res.status(200).json({
-      name: "Our NFT#" + tokenId,
+      name: "Our NFT" + tokenId,
       description: "Collection of SuperGirls",
       image: image_url + tokenId + ".svg",
     });
